25bfa010b0e82c3c931e77dc2a43a3d37b681234
jamessawyer <jamessawyer@users.noreply.github.com>
2023-02-27 06:22:11 +0000

    deploy: 2b7af18120cc42f343ece5d42c41f1b8ec9af7c3

---
+++ assets/blog_first.md.e9c95ed6.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,a}from"./app.ccfa5d92.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"blog/first.md","lastUpdated":1677478874000}'),o={name:"blog/first.md"},r=a("p",null,"first blog",-1),n=[r];function c(i,_,d,l,p,f){return s(),e("div",null,n)}const h=t(o,[["render",c]]);export{u as __pageData,h as default};
