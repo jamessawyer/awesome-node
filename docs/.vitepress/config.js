c2f37a5e5cdf3ab5df5a2c2816c8f009562dab41
jinx <550961784@qq.com>
2023-04-18 14:00:25 +0800

    book(webpack): webpack实现多线程或多进程的几个工具;happypack,thread-loader,parallel-webpack,terser等

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -127,6 +127,7 @@ const sidebar_books = {
         { text: '10.深入理解图像加载原理和最佳实践', link: '/book/webpack/10' },
         { text: '11.深入理解webpack核心配置📚', link: '/book/webpack/11' },
         { text: '13.如何使用webpack缓存提升性能⚡', link: '/book/webpack/13' },
+        { text: '14.webpack并行构建方式⚡', link: '/book/webpack/14' },
         { text: '15.开发性能极致优化技巧⚡', link: '/book/webpack/15' },
         { text: '16.使用SplitChunk提升性能⚡', link: '/book/webpack/16' },
         { text: '18.应用性能极致优化技巧⚡', link: '/book/webpack/18' },
